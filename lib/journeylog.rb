798e6e60d5da6755b2ccdffc77b08f5550de6b77
Ewan Sheldon <ewansheldon@gmail.com>
2016-08-26 12:40:28 +0100

    journey log starts journey

--- lib/journeylog.rb
+++ lib/journeylog.rb
@@ -2,6 +2,11 @@ class JourneyLog
 
   def initialize(journey_class = Journey)
     @journey_class = journey_class
+    @current_journey = nil
+  end
+
+  def start(entry_station)
+    @current_journey = @journey_class.new(entry_station)
   end
 
 end
--- spec/journeylog_spec.rb
+++ spec/journeylog_spec.rb
@@ -3,7 +3,10 @@ require 'journeylog'
 describe JourneyLog do
 
 subject(:journeylog) {described_class.new(journey_class)}
-let(:journey_class)  {double :journey_class, new: nil}
+let(:journey_class)  {double :journey_class, new: journey}
+let(:journey)        {double :journey, complete: nil}
+let(:entry_station)  {double :entry_station}
+let(:exit_station)   {double :exit_station}
 
 describe '#initialize' do
 
@@ -11,6 +14,25 @@ describe '#initialize' do
     expect(journeylog.instance_variable_get(:@journey_class)).to eq journey_class
     end
 
+  end
+
+  describe '#start' do
+    it 'creates an instance of journey class' do
+      journeylog.start(entry_station)
+      expect(journey_class).to have_received(:new).with(entry_station)
+    end
+
+    it 'saves the current journey to instance variable' do
+      journeylog.start(entry_station)
+      expect(journeylog.instance_variable_get(:@current_journey)).to eq journey
+    end
+  end
+
+  describe '#finish' do
+    it 'calls complete on the journey instance' do
+      journeylog.finish(exit_station)
+      expect(journey).to have_received(:complete).with(exit_station)
+    end
   end
 
 end
